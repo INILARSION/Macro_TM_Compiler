dfea43ffda256500d9ded444b5abb3e2bf6279a2
INILARSION <lars.karbach@web.de>
2021-10-10 21:17:26 +0200

    Fixed arg parsing

--- main.c
+++ main.c
@@ -4,17 +4,19 @@
 #include "parser.h"
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("Usage: %s [Program file] [opt. output file]", argv[0]);
-    }
-    char *program_file_path = argv[1];
+    char *program_file_path;
     char *output_file;
 
-    if (argc >= 3)
+    if (argc == 3) {
+        program_file_path = argv[1];
         output_file = argv[2];
-    else {
+    } else if (argc == 2) {
+        program_file_path = argv[1];
         output_file = calloc(strlen(program_file_path) + 5, sizeof(char));
         sprintf(output_file, "%s.out", program_file_path);
+    } else {
+        printf("Usage: %s [Program file] [opt. output file]", argv[0]);
+        exit(-1);
     }
 
     struct program *program = parse_program(program_file_path);
